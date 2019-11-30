3d432549182a687f8d6ad718545d0e2ccda6d412
3li3bdalla <ali.dev.sd@gmail.com>
2019-11-30 13:25:13 +0300

    first commit

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href=" {{ config('app.url') }} " target="_blank"></a> {{ config('app.name') }} <span
+              style="color:
+      #e27575;
+      font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">{{ setting('contact_email') }}</a></p>
+  </div>
+</footer>
